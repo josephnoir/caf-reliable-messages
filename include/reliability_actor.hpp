e20608436dc0f900b4c396928030a7fb8acdc9e2
Joseph Noir <joseph.noir@gmail.com>
2016-09-01 18:01:33 +0200

    Reworking things

--- include/reliability_actor.hpp
+++ include/reliability_actor.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <map>
 #include <tuple>
+#include <vector>
 
 #include <caf/all.hpp>
 
@@ -19,22 +19,30 @@ using register_atom  = caf::atom_constant<caf::atom("register")>;
 using send_acks_atom = caf::atom_constant<caf::atom("send_acks")>;
 
 struct reliability_state {
-  int32_t next_send = 0;
-  int32_t next_recv = 0;
-  int32_t last_acked = 0;
-  std::vector<int32_t> skipped;
-  std::map<int32_t,reliable_msg> outbox;  // requires receiving acks from other
-  std::map<int32_t,reliable_msg> inbox;   // received, but missing previous seq
+  int16_t unacked = 0;
+  int32_t seq_send = 0;
+  int32_t seq_recv = 0;
+  std::vector<reliable_msg> inbox;   // missing previous seq
+  std::vector<reliable_msg> outbox;  // requires acks from dest
   std::string name = "reliability_actor";
 };
 
-std::tuple<int32_t, int32_t, std::array<int32_t,3>>
-find_acks(reliability_state& state);
+reliable_msg create_ack_msg(reliability_state& state);
 
+std::vector<reliable_msg>::iterator find_seq(std::vector<reliable_msg>& vec,
+                                             const int32_t seq);
+
+/// Actor doesn't know the broker yet, waiting to be initialized
 caf::behavior init_reliability_actor(caf::stateful_actor<reliability_state>* self,
-                                         const caf::actor& buddy);
+                                     const caf::actor& app);
+
+/// Actor know the application and the broker, working state
+/// Functionality:
+/// - retransmit / ack
+/// - ordering
+/// - missing: duplicate packet detection
 caf::behavior reliability_actor(caf::stateful_actor<reliability_state>* self,
-                                const caf::actor& buddy,
+                                const caf::actor& app,
                                 const caf::actor& broker);
 
 } // namespace relm
--- src/reliability_actor.cpp
+++ src/reliability_actor.cpp
@@ -15,54 +15,71 @@ namespace relm {
 namespace {
 auto default_timeout = milliseconds(300);
 auto forward_delay = milliseconds(50);
-auto ack_interval = milliseconds(200);
+auto ack_interval_time = milliseconds(200);
+int16_t ack_interval_count = 50;
 }
 
-tuple<int32_t, int32_t, array<int32_t,3>> find_acks(reliability_state& state) {
-  auto & inbox = state.inbox;
-  int32_t highest = state.last_acked;
-//  int32_t searching = highest + 1;
+reliable_msg create_ack_msg(reliability_state& state) {
+  int32_t highest = state.seq_recv;
+  int32_t searching = highest + 1;
   int32_t found = 0;
   size_t idx = 0;
   std::array<int32_t,3> nacks = {{0,0,0}};
-  while (found < 3 && idx < inbox.size()) {
+  while (found < 3 && idx < state.inbox.size()) {
+    auto itr = find_seq(state.inbox, searching);
+    if (itr != state.inbox.end()) {
+      
+    }
+    ++searching;
     ++idx;
   }
-  return make_tuple(highest, found, nacks);
+  return reliable_msg::ack(highest, found, nacks);
+}
+
+vector<reliable_msg>::iterator find_seq(vector<reliable_msg>& vec,
+                                        const int32_t seq) {
+  return find_if(begin(vec), end(vec), [seq](const reliable_msg& msg) {
+    return msg.seq == seq;
+  });
 }
 
 behavior init_reliability_actor(stateful_actor<reliability_state>* self,
-                                    const actor& buddy) {
+                                    const actor& app) {
   aout(self) << "Bootstrapping, awaiting message from broker" << endl;
   self->set_default_handler(skip);
   return {
     [=] (register_atom, const actor& broker) {
-      self->become(reliability_actor(self, buddy, broker));
+      self->become(reliability_actor(self, app, broker));
     }
   };
 }
 
+void send_acks(stateful_actor<reliability_state>* self,
+               const actor& broker) {
+  auto ack_msg = create_ack_msg(self->state);
+  self->send(broker, send_atom::value, ack_msg);
+}
+
 behavior reliability_actor(stateful_actor<reliability_state>* self,
-                           const actor& buddy, const actor& broker) {
+                           const actor& app, const actor& broker) {
   self->set_default_handler(print_and_drop);
-  self->delayed_send(self, ack_interval, send_acks_atom::value);
+  self->delayed_send(self, ack_interval_time, send_acks_atom::value);
   aout(self) << "[R] Bootstrapping done, now running." << endl;
   return {
-    [=](ack_atom, int32_t seq) {
+    [=](ack_atom, int32_t seq, int32_t num_nacks, array<int32_t,3> nacks) {
+      // ack all le to seq
       auto& outbox = self->state.outbox;
-      auto msg_itr = outbox.find(seq);
-      if (msg_itr != end(outbox)) {
-        aout(self) << "[R][" << seq << "] Received ACK." << endl;
-        outbox.erase(msg_itr);
-      } else {
-        aout(self) << "[R][" << seq << "] Received out of date ACK." << endl;
-      }
+      aout(self) << "[R][" << seq << "] Received ACK." << endl;
+      erase(remove_if(begin(outbox), end(outbox),
+                      [seq,num_nacks,nacks](const reliable_msg& msg) {
+        return msg.seq <= seq 
+      }));
     },
     [=](retransmit_atom, int32_t seq) {
       // May be easier to look for the next retransmit and set a timer
       // for it instead of using a separate timers for each possible retransmit.
       auto& outbox = self->state.outbox;
-      auto msg_itr = outbox.find(seq);
+      auto msg_itr = find_seq(outbox, seq);
       if (msg_itr != end(outbox)) {
         self->send(broker, send_atom::value, *msg_itr);
         aout(self) << "[R][" << seq << "] Retransmitting." << endl;
@@ -72,65 +89,63 @@ behavior reliability_actor(stateful_actor<reliability_state>* self,
       }
     },
     [=](send_acks_atom) {
-      // BEGIN: testing ack messages
-      int32_t highest;
-      int32_t num_nacks;
-      array<int32_t, 3> nacks;
-      tie(highest, num_nacks, nacks) = find_acks(self->state);
-      aout(self) << "[ACKS]  highest: " << highest << ", number of nacks: "
-                 << num_nacks << ", nacks: [" << nacks[0] << ", " << nacks[1]
-                 << ", " << nacks[2] << "]" << endl;
-      // END: testing ack messages
-      self->delayed_send(self, ack_interval, send_acks_atom::value);
+      // a time trigger for sending acks
+      send_acks(self, broker);
+      self->delayed_send(self, ack_interval_time, send_acks_atom::value, true);
     },
     [=](recv_atom, const reliable_msg& msg) {
       // Incoming message
       if (msg.atm == ping_atom::value || msg.atm == pong_atom::value) {
-        // APPLICATION msg
-        auto& next = self->state.next_recv;
+        // --> APPLICATION
+        self->state.unacked += 1;
+        auto next = self->state.seq_recv + 1;
         if (msg.seq < next) {
           aout(self) <<  "[R][" << msg.seq << "] Message " << to_string(msg)
                    << " has old sequence number, adding to pending." << endl;
-          // will be acked automatically, by cumutative acks
+          // Sender did not receive ack yet, will be acked automatically
         } else if (msg.seq == next) {
           aout(self) <<  "[R][" << msg.seq << "] Received " << to_string(msg)
                      << "." << endl;
-          next += 1;
-          self->delayed_send(buddy, forward_delay, msg.atm, msg.content);
+          self->delayed_send(app, forward_delay, msg.atm, msg.content);
           // ACK will be sent by "send_ack_atom" handler, expecting that all
           // seqs < next_recv have been received ...
           // look for received messages with subsequent sequence numbers
           auto& early = self->state.inbox;
-          auto msg_itr = early.find(next);
+          auto msg_itr = find_seq(early, next);
           while (msg_itr != end(early)) {
-            self->delayed_send(buddy, forward_delay, msg.atm, msg.content);
+            self->delayed_send(app, forward_delay, msg.atm, msg.content);
             early.erase(msg_itr);
             next += 1;
-            msg_itr = early.find(next);
+            msg_itr = find_seq(early, next);
           }
+          self->state.seq_recv += next;
         } else if (msg.seq > next) {
           aout(self) << "[R][" << msg.seq << "] Message " << to_string(msg.atm)
-                     << " was early, awaiting "
-                     << next << "." << endl;
-          self->state.inbox.emplace(msg.seq, msg);
+                     << " was early, awaiting " << next << "." << endl;
+          self->state.inbox.emplace_back(msg);
+        }
+        if (self->state.unacked >= ack_interval_count) {
+          // TODO: do some acking
+          // self->send(self, send_acks_atom::value);
+          send_acks(self, broker);
         }
       } else {
-        // CONTROL msg
+        // --> CONTROL
         self->send(self, msg.atm, msg.seq);
       }
     },
     [=](atom_value av, int32_t i) {
       // Message from ping actor, forward via our connection handle
       assert(av == ping_atom::value || av == pong_atom::value);
-      auto& next = self->state.next_send;
+      auto next = self->state.seq_send + 1;
       int32_t seq = next;
       auto msg = reliable_msg::msg(av, i, seq);
       aout(self) << "[R][" << seq << "] Sending " << to_string(msg)
                  << "." << endl;
       self->send(broker, send_atom::value, msg);
-      self->state.outbox.emplace(msg.seq,msg);
+      self->state.outbox.emplace_back(msg);
       self->delayed_send(self, default_timeout, retransmit_atom::value, seq);
-      next += 1; // increase sequence number for next packet
+      self->state.seq_send = next; // increase sequence number for next packet
     }
   };
 }
